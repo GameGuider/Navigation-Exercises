ca50c02f17300532376b3030f4472254faa12e73
Zenenko Kirill <kirill255pro@gmail.com>
2019-04-01 23:44:19 +1000

    Add files via upload

---
+++ Navigation Exercises.py
@@ -0,0 +1,24 @@
+TC = input("Введите ИК: ")
+CC = input("Введите КК: ")
+MC = input("Введите МК: ")
+GCC = input("Введите ГКК: ")
+CCor = input("Введите △К: ")
+GCCor = input("Введите △ГК: ")
+
+TB = input("Введите ИП: ")
+CB = input("Введите КП: ")
+MB = input("Введите МП: ")
+GCB = input("Введите ГКП: ")
+
+d = input("Введите d: ")
+
+TA = input("Введите ПУ: ")
+
+
+TC = float(CC) + float(CCor)
+TC = float(GCC) + float(GCCor)
+TC = float(CC) + float(CCor)
+TC = float(CC) + float(CCor)
+
+
+print(TC)
